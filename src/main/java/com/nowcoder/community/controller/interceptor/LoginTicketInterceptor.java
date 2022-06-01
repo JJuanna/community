1ad866a7a1c4c39eae2a331d55e6dbe203af73bf
DuJuanjuan <1290723581@qq.com>
2022-06-01 16:42:57 +0800

    显示登录信息

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/LoginTicketInterceptor.java
@@ -0,0 +1,61 @@
+package com.nowcoder.community.controller.interceptor;
+
+import com.nowcoder.community.entity.LoginTicket;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CookieUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Component
+public class LoginTicketInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    // 在一开始请求的过程中，随时都要用到用户数据，所以在controller之前做
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        return HandlerInterceptor.super.preHandle(request, response, handler);
+        // cookie从request中传过来,从cookie中获取凭证
+        String ticket = CookieUtil.getValue(request, "ticket");
+        if (ticket != null) {
+            // 查询凭证
+            LoginTicket loginTicket = userService.findLoginTicket(ticket);
+            // 检查凭证是否有效:loginTicket不为空&&状态有效&&登录凭证未超时
+            if (loginTicket != null && loginTicket.getStatus() == 0 && loginTicket.getExpired().after(new Date())) {
+                // 根据凭证查询用户
+                User user = userService.findUserById(loginTicket.getUserId());
+                // 在本次请求中持有用户(查到用户之后要在后面用，因此我们查到之后需要暂存，浏览器访问服务器是并发的，每个浏览器访问服务器是一个线程，
+                // 因此我们要考虑线程的隔离)
+                hostHolder.setUser(user);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if (user != null && modelAndView != null) {
+            // tymeleaf识别不到这个属性名
+            modelAndView.addObject("loginUser", user);
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 清除用户
+        hostHolder.clear();
+    }
+}
